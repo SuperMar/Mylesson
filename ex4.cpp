fd5da29ae093888d90bc58d71484da46b22ce9db
SuperMar <mahuanreg@qq.com>
2017-02-18 15:02:43 +0800

    Update ex4.cpp

--- ex4.cpp
+++ ex4.cpp
@@ -2,7 +2,7 @@
 int main()
 {
   int sum = 0;
-  for(int val = 0; val <= 10;++val)
+  for(int val = 0; val <= 10; ++val)
     sum += val;
   std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl;
   return 0;
