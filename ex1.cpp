c2eec29ae68af23bf740dc82be5c40c19f618c28
SuperMar <mahuanreg@qq.com>
2017-02-18 15:28:28 +0800

    Update ex1.cpp

--- ex1.cpp
+++ ex1.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 int main()
 {
-  int a = 0, b = 0, c;
+  int a = 0, b = 0, c, d, n;
   std::cout << "Enter two numbers: " << std::endl; 
   std::cin >> a >> b;
   c = a < b ? a : b;
-  while(c >= a && c <= b){
-    std::cout << c << std::endl;
+  d = a > b ? a : b;
+  while(n >= c && n <= d){
+    std::cout << n << std::endl;
     ++c;
   }
   return 0;
