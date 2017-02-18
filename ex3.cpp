1f675cf3507ed0d87cb054cc69e4adf40caec796
SuperMar <mahuanreg@qq.com>
2017-02-18 14:52:28 +0800

    Update ex3.cpp

--- ex3.cpp
+++ ex3.cpp
@@ -4,3 +4,8 @@ int main()
   int val = 0;
   std::cout << (std::cin >> var) << std::endl; // (wrong) std::cin (返回类型void*)正常读入返回 &istream, 遇错返回0
 }
+
+//std::cout << "/*"
+//std::cout << "*/"
+//std::cout << /* "*/" */
+//std::cout << /* "*/" /* "/*" */
