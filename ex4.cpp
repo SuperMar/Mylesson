35f9cfe88c00bab2d9afbda5537d49267862a1c2
SuperMar <mahuanreg@qq.com>
2017-02-18 15:07:29 +0800

    Update ex4.cpp

--- ex4.cpp
+++ ex4.cpp
@@ -2,7 +2,7 @@
 int main()
 {
   int sum = 0;
-  for(int val = 0; val <= 10; ++val)
+  for(int val = 0; val <= 10; ++val) //变量val仅在循环入口出执行一次，循环结束后不能使用
     sum += val;
   std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl;
   return 0;
