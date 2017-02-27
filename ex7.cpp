e5bc5fb92f3a7526718ae688a935ee8f762e65bd
SuperMar <mahuanreg@qq.com>
2017-02-27 22:22:24 +0800

    Update ex7.cpp

--- ex7.cpp
+++ ex7.cpp
@@ -1,19 +1,19 @@
-C++基本形态:内置数据类型,自定义数据类型+运算符+程序流控制语句
+C++基本形态:数据类型(内置数据类型,自定义数据类型)+运算符+程序流控制语句
 数据类型包括基本数据类型和自定义数据类型->基本数据类型包括算数类型和空类型->算数类型包括整型和浮点型->整型包括字符型,整型和bool类型
 算术类型包括以下全部:
-char
+char(最小8bit)
 unsigned char
 signed char
-wchar_t
-char16_t
-char32_t
-unsigned/signed
-short
-int
-long
-long long
-float
-double
-long double
+wchar_t(最小16bit)
+char16_t(最小16bit)
+char32_t(最小32bit)
+unsigned/signed:
+short(最小16bit)
+int(最小16bit)
+long(最小32bit)
+long long(最小64bit)
+float(6位有效数字)
+double(10位有效数字)
+long double(10位有效数字)
 bool(True/False)
 void
